6369e0ccc9e3702468a92c3a9145c593b4edf162
廖韋臣 <rax02270521@gmail.com>
2020-12-14 22:21:01 +0800

    顯示已有任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -12,7 +13,10 @@ class TaskController extends Controller
     }
     public function index(Request &$request)
     {
-        return view('tasks.index');
+        $tasks = Task::where('user_id',$request->user()->id)->get();
+        return view('tasks.index',[
+            'tasks'=>$tasks,
+        ]);
     }
     public function store(Request $request)
     {
