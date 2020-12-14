031a1a3b37b889d4359f4f26fb83819fb595dc9b
廖韋臣 <rax02270521@gmail.com>
2020-12-14 20:40:00 +0800

    準備資料庫

---
+++ database/migrations/2020_12_14_114313_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('takes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('user_id')->index();
+            $table->string('name');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('takes');
+    }
+}
